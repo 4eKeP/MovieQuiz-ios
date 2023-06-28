64436c5a5b89edc68eda3c61c4c4101d8079d71c
Egor <egor_chek@mail.ru>
2023-06-28 21:53:51 +0400

    реализован интерфейс(2 кнопки, текст вопроса, изображение фильма, поле "вопрос", счетчик) добавлены шрифты. в MovieQuizViewController добавлена модель вопроса и модель отображения, добавлен массив с mock данными, реализована функция отображение первого вопроса при запуске, и функция отображения правильного ответа с помощью цвета рамки изображения.

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,10 +1,121 @@
 import UIKit
 
+struct QuizQuestion{
+    //название картинки(фильма) из assets
+    let image: String
+    //строка с вопросом о рейтинге фильма
+    let text: String
+    //значение ответа
+    let correctAnswer: Bool
+}
+
+struct QuizStepViewModel {
+    //картинка фильма
+    let image: UIImage
+    //вопрос о рейтинге фильма
+    let question: String
+    //порядковый номер вопроса "1/10"
+    let questionNumber: String
+}
+
+
+
 final class MovieQuizViewController: UIViewController {
+    
+    private var currentQuestionIndex = 0
+    
+    private var correctAnswers = 0
+    
+    private let questions: [QuizQuestion] = [
+        QuizQuestion(image: "The Godfather",
+                     text: "Рейтинг этого фильма больше чем 6?",
+                     correctAnswer: true),
+        QuizQuestion(image: "The Dark Knight",
+                     text: "Рейтинг этого фильма больше чем 6?",
+                     correctAnswer: true),
+        QuizQuestion(image: "Kill Bill",
+                     text: "Рейтинг этого фильма больше чем 6?",
+                     correctAnswer: true),
+        QuizQuestion(image: "The Avengers",
+                     text: "Рейтинг этого фильма больше чем 6?",
+                     correctAnswer: true),
+        QuizQuestion(image: "Deadpool",
+                     text: "Рейтинг этого фильма больше чем 6?",
+                     correctAnswer: true),
+        QuizQuestion(image: "The Green Knight",
+                     text: "Рейтинг этого фильма больше чем 6?",
+                     correctAnswer: true),
+        QuizQuestion(image: "Old",
+                     text: "Рейтинг этого фильма больше чем 6?",
+                     correctAnswer: false),
+        QuizQuestion(image: "The Ice Age Adventures of Buck Wild",
+                     text: "Рейтинг этого фильма больше чем 6?",
+                     correctAnswer: false),
+        QuizQuestion(image: "Tesla",
+                     text: "Рейтинг этого фильма больше чем 6?",
+                     correctAnswer: false),
+        QuizQuestion(image: "Vivarium",
+                     text: "Рейтинг этого фильма больше чем 6?",
+                     correctAnswer: false),
+    ]
+    
     // MARK: - Lifecycle
+    
+    @IBOutlet private weak var questionTitleLabel: UILabel!
+    @IBOutlet private weak var counterLabel: UILabel!
+    @IBOutlet private weak var imageView: UIImageView!
+    @IBOutlet private weak var questionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        imageView.layer.cornerRadius = 20
+        show(quiz: convert(model: questions[currentQuestionIndex]))
     }
+    
+    @IBAction private func noButtonPressed(_ sender: UIButton) {
+        let userAnswer = questions[currentQuestionIndex].correctAnswer == false
+        showAnswerResult(isCorrect: userAnswer)
+    }
+    
+    @IBAction private func yesButtonPressed(_ sender: UIButton) {
+        let userAnswer = questions[currentQuestionIndex].correctAnswer == true
+        showAnswerResult(isCorrect: userAnswer)
+    }
+    
+    private func showAnswerResult(isCorrect: Bool){
+        imageView.layer.masksToBounds = true
+        imageView.layer.borderWidth = 8
+        imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
+        imageView.layer.cornerRadius = 20
+//        if isCorrect == true{
+//            yesButtonBorder()
+//            correctAnswers += 1
+//        }else{
+//            noButtonBorder()
+//        }
+//        currentQuestionIndex += 1
+//        show(quiz: convert(model: questions[currentQuestionIndex]))
+    }
+    
+//    private func ButtonBorder(){
+//
+//    }
+    
+    private func show(quiz step: QuizStepViewModel){
+        imageView.image = step.image
+        questionLabel.text = step.question
+        counterLabel.text = step.questionNumber
+    }
+    
+    private func convert(model: QuizQuestion) -> QuizStepViewModel {
+        QuizStepViewModel(image: UIImage(named: model.image) ?? UIImage(),
+                          question: model.text,
+                          questionNumber: "\(currentQuestionIndex + 1)/\(questions.count)")
+    }
+    
+   
+    
+    
 }
 
 /*
